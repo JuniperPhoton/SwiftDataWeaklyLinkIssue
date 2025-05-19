1e33e6224f3c6aef8cc48baecef9bca96b910783
dengweichao <dengweichao@hotmail.com>
2025-05-20 01:04:19 +0800

    Initial Commit

---
+++ SwiftDataCompatTest/SwiftDataCompatTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataCompatTestApp.swift
+//  SwiftDataCompatTest
+//
+//  Created by JuniperPhoton on 2025/5/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataCompatTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
